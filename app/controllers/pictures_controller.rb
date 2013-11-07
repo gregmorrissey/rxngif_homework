fbc83c8700b18191a4982592490a39e2444f68fc
Greg A Morrissey <greg-morrissey@kellogg.northwestern.edu>
2013-11-07 03:14:41 -0600

    Homework Complete

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,40 @@
 class PicturesController < ApplicationController
   def show
+  	@picture = Picture.find(params[:id])
+  end
+
+  def index
+  	@list_of_pictures = Picture.all
+  end
+
+  def new
+  	render "new"
+  end
+
+  def create
+  	i = Picture.new
+	i.source = params[:source]
+	i.caption = params[:caption]
+	i.save
+  	render "create"
+  end
+
+  def destroy
+	i = Picture.find(params[:id])
+	i.destroy
+  	render "destroy"
+  end
+
+  def edit
+  	@picture = Picture.find(params[:id])
+  	render "edit"
+  end
+
+  def update
+  	i = Picture.find(params[:id])
+  	i.source = params[:source]
+  	i.caption = params[:caption]
+  	i.save
+  	render "update"
   end
 end
