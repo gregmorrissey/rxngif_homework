0485b3b9d3ead899ae7f8a22dbcb86ea844dc31d
Greg A Morrissey <greg-morrissey@kellogg.northwestern.edu>
2013-11-12 00:42:39 -0600

    HW Wk 7 Partial

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,40 +1,40 @@
 class PicturesController < ApplicationController
   def show
-  	@picture = Picture.find(params[:id])
+  	 @picture = Picture.find(params[:id])
   end
 
   def index
-  	@list_of_pictures = Picture.all
+  	 @list_of_pictures = Picture.all
   end
 
   def new
-  	render "new"
+  	 render "new"
   end
 
   def create
-  	i = Picture.new
-	i.source = params[:source]
-	i.caption = params[:caption]
-	i.save
-  	render "create"
+  	 i = Picture.new
+	   i.source = params[:source]
+	   i.caption = params[:caption]
+	   i.save
+  	 redirect_to "http://localhost:3000/all_pictures"
   end
 
   def destroy
-	i = Picture.find(params[:id])
-	i.destroy
-  	render "destroy"
+	   i = Picture.find(params[:id])
+	   i.destroy
+  	 redirect_to "http://localhost:3000/all_pictures"
   end
 
   def edit
-  	@picture = Picture.find(params[:id])
-  	render "edit"
+  	 @picture = Picture.find(params[:id])
+  	 render "edit"
   end
 
   def update
-  	i = Picture.find(params[:id])
-  	i.source = params[:source]
-  	i.caption = params[:caption]
-  	i.save
-  	render "update"
+  	 i = Picture.find(params[:id])
+  	 i.source = params[:source]
+  	 i.caption = params[:caption]
+  	 i.save
+  	 redirect_to "http://localhost:3000/picture_details/#{i.id}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rxngif::Application.routes.draw do
-  get("/picture_details/:id", { :controller => "pictures", :action => "show" })
+  	get("/picture_details/:id", { :controller => "pictures", :action => "show" })
+  	get("/all_pictures", {:controller => "pictures", :action => "index"})
 
-  get("/all_pictures", {:controller => "pictures", :action => "index"})
+  	get("/new_picture_form", { :controller => "pictures", :action => "new", :as => "new_picture"})
+	get("/create_picture", { :controller => "pictures", :action => "create" })
 
-  get("/new_picture_form", { :controller => "pictures", :action => "new"})
-
-  get("/create_picture", { :controller => "pictures", :action => "create" })
-
-	get("/delete_picture/:id", { :controller => "pictures", :action => "destroy" })
+	delete("/delete_picture/:id", { :controller => "pictures", :action => "destroy" })
 
 	get("/edit_picture_form/:id", { :controller => "pictures", :action => "edit" })
-
 	get("/update_picture/:id", { :controller => "pictures", :action => "update" })
 end
